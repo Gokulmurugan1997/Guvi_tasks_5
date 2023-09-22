4f38554f5e0e20f03c7bf62d568e7b65627c3a64
Gokulmurugan1997 <Gokulanshvi@gmail.com>
2023-09-22 11:54:50 +0530

    update

--- circle/circle.js
+++ circle/circle.js
@@ -1,8 +1,33 @@
-class circle{
+class Circle{
     constructor (radius,color){
-            this.radius=2;
-            this.color="red";
+            this.radius=radius;
+            this.color=color;
+            this.pie=3.14;
 
-            
+    }
+    getRadius(){
+        return(`the radius of circle is ${this.radius}`)
+    }
+    setRadius(rad){
+        this.radius=rad;
+    }
+    getColor(){
+        return (`the color of circle is ${this.color}`);
+    }
+    setColor (col){
+        this.color=col;
+    }
+    getArea(){
+        return(`The area of circle is ${this.pie*this.radius*this.radius}`)
+    }
+    getCirumferencce(){
+        return(`The circumference of circle is ${2*this.pie*this.radius}`)
     }
 }
+ const circle= new Circle(1,"red");
+ circle.setRadius(2);
+ circle.setColor("white");
+ console.log(circle.getArea());
+ console.log(circle.getCirumferencce());
+ console.log(circle.getColor());
+ console.log(circle.getRadius());
